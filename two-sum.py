ec8c814e9e17bd5c9cae51d1a25a9c501b282f82
Bego <your-email@example.com>
2026-03-27 22:15:34 +0300

    solve: Two Sum

---
+++ two-sum.py
@@ -0,0 +1,10 @@
+# Two Sum problem
+
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
