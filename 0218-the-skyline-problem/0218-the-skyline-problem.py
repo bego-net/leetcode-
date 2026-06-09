5777adb65fbcbade1e8d1b7a831bbf698b0d1661
Begonet Debebe <begonetdebebe@gmail.com>
2026-06-09 14:34:41 +0300

    Time: 21 ms (63.58%), Space: 26.8 MB (57.75%) - LeetHub

---
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    def getSkyline(self, buildings):
+        events = []
+
+        for l, r, h in buildings:
+            events.append((l, -h, r))  # start
+            events.append((r, 0, 0))   # end marker
+
+        events.sort()
+
+        res = []
+        heap = [(0, float('inf'))]  # (-height, right)
+
+        for x, neg_h, r in events:
+
+            while heap and heap[0][1] <= x:
+                heapq.heappop(heap)
+
+            if neg_h:
+                heapq.heappush(heap, (neg_h, r))
+
+            curr_height = -heap[0][0]
+
+            if not res or res[-1][1] != curr_height:
+                res.append([x, curr_height])
+
+        return res
