ee506701f07f4c39c9055d7401a1c1506212609c
Begonet Debebe <begonetdebebe@gmail.com>
2026-06-20 00:35:04 +0300

    Time: 11 ms (67.12%), Space: 19.7 MB (68.43%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+
+        cols = set()
+        diag1 = set()  # row - col
+        diag2 = set()  # row + col
+
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(row):
+            if row == n:
+                res.append(["".join(r) for r in board])
+                return
+
+            for col in range(n):
+                if (col in cols or
+                    row - col in diag1 or
+                    row + col in diag2):
+                    continue
+
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+                board[row][col] = "Q"
+
+                backtrack(row + 1)
+
+                board[row][col] = "."
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+        return res
